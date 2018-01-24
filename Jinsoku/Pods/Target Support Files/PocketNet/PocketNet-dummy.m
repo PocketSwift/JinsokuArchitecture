2e787bded62d3bc9cd002df44832e4c849a5df40
Jose Antonio Garcia Yañez <joseantoniogarciay@gmail.com>
2018-01-24 09:44:43 +0100

    pods installed

---
+++ Jinsoku/Pods/Target Support Files/PocketNet/PocketNet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PocketNet : NSObject
+@end
+@implementation PodsDummy_PocketNet
+@end
---
+++ Jinsoku/Pods/Target Support Files/Pods-Jinsoku/Pods-Jinsoku-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Jinsoku : NSObject
+@end
+@implementation PodsDummy_Pods_Jinsoku
+@end
